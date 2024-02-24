0dfc79b06b0a3a2d4afb7a46b5fc4db0976cd373
dhruv <dr26dhruv@gmail.com>
2024-02-24 13:31:27 +0530

    app.js updated - category router

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ app.use(cookieParser());
 // routes import
 import userRouter from "./routes/user.routes.js";
 import reviewRouter from './routes/review.routes.js'
+import categoryRouter from './routes/category.routes.js'
 
 
 
@@ -27,5 +28,6 @@ import reviewRouter from './routes/review.routes.js'
 //routes declaration 
 app.use("/api/v1/users", userRouter);
 app.use('/api/v1/review',reviewRouter)
+app.use('/api/v1/category',categoryRouter)
 
 export { app };
