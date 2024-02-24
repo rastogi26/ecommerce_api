0341aeda78ee93e5b41b4543ea6811194cdaef5a
dhruv <dr26dhruv@gmail.com>
2024-02-24 12:53:36 +0530

    bug fixed in review route

--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import verifyJWT from "../middlewares/auth.middleware";
-import { submitReview } from "../controllers/review.controller";
+import verifyJWT from "../middlewares/auth.middleware.js";
+import { submitReview } from "../controllers/review.controller.js";
 
 const router = Router();
 
