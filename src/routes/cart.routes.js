2faf584be1fdfba005c414f0f9922e932a06aa4a
dhruv <dr26dhruv@gmail.com>
2024-02-24 15:43:18 +0530

    cart routes done

---
+++ src/routes/cart.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  addToCart,
+  getCart,
+  removeCartItem,
+  updateCartItem,
+} from "../controllers/cart.controller";
+
+const router = Router();
+
+router.route("/add-to-cart").post(verifyJWT, addToCart);
+router.route("/get-cart").get(verifyJWT, getCart);
+router.route("/update-cart-item").put(verifyJWT, updateCartItem);
+router.route("/remove/:productId").delete(verifyJWT, removeCartItem);
+
+export default router;
