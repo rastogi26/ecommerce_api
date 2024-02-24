ef092ab2db7008a0884b44f5c72e5ca7ce0a7f7b
dhruv <dr26dhruv@gmail.com>
2024-02-24 14:46:41 +0530

    product routes done

---
+++ src/routes/product.routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import {
+    getProductDetails,
+  getProductList,
+  registerProduct,
+} from "../controllers/product.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import isAdmin from "../middlewares/isAdmin.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
+const router = Router();
+
+router.route("/register-product").post(
+  verifyJWT, //check user login or not,
+  isAdmin, // check if user is admin or not
+  upload.single("image"),
+  registerProduct
+);
+
+router.route("/:categoryId").get(verifyJWT, getProductList);
+router.route("/:productId").get(verifyJWT,getProductDetails)
+
+export default router;
