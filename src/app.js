0d125ceec2fddce29a77eaaa37a91cbae2250d9c
dhruv <dr26dhruv@gmail.com>
2024-02-24 15:43:46 +0530

    app.js updated with cart router

--- src/app.js
+++ src/app.js
@@ -19,17 +19,16 @@ app.use(cookieParser());
 
 // routes import
 import userRouter from "./routes/user.routes.js";
-import reviewRouter from './routes/review.routes.js'
-import categoryRouter from './routes/category.routes.js'
-import productRouter from './routes/product.routes.js'
+import reviewRouter from "./routes/review.routes.js";
+import categoryRouter from "./routes/category.routes.js";
+import productRouter from "./routes/product.routes.js";
+import cartRouter from "./routes/cart.routes.js";
 
-
-
-
-//routes declaration 
+//routes declaration
 app.use("/api/v1/users", userRouter);
-app.use('/api/v1/review',reviewRouter)
-app.use('/api/v1/category',categoryRouter)
-app.use('/api/v1/products',productRouter)
+app.use("/api/v1/review", reviewRouter);
+app.use("/api/v1/category", categoryRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/cart", cartRouter);
 
 export { app };
